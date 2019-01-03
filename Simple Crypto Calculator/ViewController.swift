1f87331f6d919c1675ecf332ce9d6452cd192705
George <george@Georges-Mac.local>
2019-01-03 18:11:40 +0000

    Initial Commit

---
+++ Simple Crypto Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Crypto Calculator
+//
+//  Created by George on 03/01/2019.
+//  Copyright © 2019 George. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
