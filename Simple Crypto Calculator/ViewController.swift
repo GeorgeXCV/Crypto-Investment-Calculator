c8c38cbd22f5b4892e43ad652857442892008cac
GeorgeXCV <georgeashton95@gmail.com>
2019-03-15 20:09:17 +0000

    Calculations done

--- Simple Crypto Calculator/ViewController.swift
+++ Simple Crypto Calculator/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var investAmount: UITextField!
     @IBOutlet var worthAmount: UITextField!
     @IBOutlet var wouldBe: UILabel!
-    
     @IBOutlet var cryptoNameButton: UIButton!
     
     // Store JSON results here
@@ -24,10 +23,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     // Empty property to store calcuations
     var coinsBought: Double = 0
-    var totalValue: Double?
+    var totalValue: Double = 0
     
     
-        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -39,59 +37,34 @@ class ViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
-    
-    
-    
+    // When investment amount eneted
     @IBAction func investmentEntered(_ sender: Any) {
+        // If text value exists, perform below code
         guard let text = investAmount.text else {
             return
         }
-        if let invest = Double(text) {
-         if let coinPrice = cryptoPrice {
-            coinsBought = invest / coinPrice
+        if let invest = Double(text) { // If we can convert value of textfield to Double
+         if let coinPrice = cryptoPrice { // And if cryptoPrice has a value
+            coinsBought = invest / coinPrice // Investment amount / current coin price = How many coins we would buy
         }
     }
-    }
+}
     
-    // Investment / current coin price = How many coins you would get
-    // Amount of coins * Projected value per coin = Investment worth
+    // When projected value of coin entered
+    @IBAction func projectionEntered(_ sender: Any) {
+        // If both text fields have text, run the code below
+        guard let text = worthAmount.text, let text2 = investAmount.text else {
+            return
+        }
+        
+        if let projection = Double(text) { // If worthAmount has text to convert to Double
+            totalValue = coinsBought * projection    // Amount of coins we can buy * Projected value per coin = Investment worth
+            wouldBe.text = "\(totalValue)" // Add the total value to investment worth label
+     }
+    }
+ 
     // If I invest £100 in EOS which is £1.85 a coin, £100 / £1.85 = 54 Coins. If value per coin goes up to £10, (54 * 10), my investment is worth £540
     
-    
-    
-//    func textFieldDidEndEditing(_ textField: UITextField) {
-//        guard let text = investAmount.text, let textTwo = worthAmount.text else {
-//        return
-//    }
-//        if let invest = Double(text) {
-//           if let worth = Double(textTwo) {
-//            if let price = cryptoPrice {
-//                    var amountOfCoins = invest / price
-//                    totalValue = amountOfCoins * worth
-//                    print(totalValue)
-//                    }
-//            }
-//        }
-//    }
-    
-//    func textFieldDidBeginEditing(_ textField: UITextField) {
-//        guard let text = investAmount.text else {
-//            return
-//        }
-//
-//        if let invest = Double(text) {
-//            if let price = cryptoPrice {
-//               coinsBought = invest / price
-//            }
-//        }
-//    }
-
-    
-    
-   
-    
-    
-
 }
 
 
