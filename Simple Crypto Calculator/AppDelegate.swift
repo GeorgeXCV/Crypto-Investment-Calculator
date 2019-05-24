e08503eddd38021baf00f3f84d8e8c1b2ec7a1e7
GeorgeXCV <georgeashton95@gmail.com>
2019-05-24 21:30:59 +0100

    Revert back here if screw up Autolayout

--- Simple Crypto Calculator/AppDelegate.swift
+++ Simple Crypto Calculator/AppDelegate.swift
@@ -20,7 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
        
-        GADMobileAds.configure(withApplicationID: "ca-app-pub-3940256099942544/2934735716")
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+
 
         
         return true
